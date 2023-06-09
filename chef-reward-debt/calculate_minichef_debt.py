071b408877cb8a18381092db97219253a18d019f
0xJiro <jiro@sushi.com>
2023-06-09 19:20:39 -0400

    updated minichef scripts

--- chef-reward-debt/calculate_minichef_debt.py
+++ chef-reward-debt/calculate_minichef_debt.py
@@ -7,7 +7,7 @@ from web3 import Web3, HTTPProvider
 
 GRAPH_ENDPOINTS = {
     'polygon': 'https://api.thegraph.com/subgraphs/name/sushiswap/matic-minichef',
-    'xdai': 'https://api.thegraph.com/subgraphs/name/matthewlilley/jiro-ono/gnosis-minichef-staging',
+    'gnosis': 'https://api.thegraph.com/subgraphs/name/sushiswap/minichef-gnosis',
     'harmony': 'https://sushi.graph.t.hmny.io/subgraphs/name/sushiswap/harmony-minichef',
     'celo': 'https://api.thegraph.com/subgraphs/name/sushiswap/celo-minichef-v2',
     'moonriver': 'https://api.thegraph.com/subgraphs/name/sushiswap/moonriver-minichef',
@@ -17,12 +17,13 @@ GRAPH_ENDPOINTS = {
 RPC_ENDPOINTS = {
     'mainnet': 'https://mainnet.infura.io/v3/',
     'polygon': 'https://polygon-rpc.com/',
-    'xdai': 'https://rpc.xdaichain.com',
+    'gnosis': 'https://rpc.gnosischain.com',
     'harmony': 'https://api.harmony.one',
     'celo': 'https://forno.celo.org',
     'moonriver': 'https://rpc.moonriver.moonbeam.network',
     'arbitrum': 'https://arb1.arbitrum.io/rpc'
 }
+
 
 def main(chain):
 
@@ -57,20 +58,24 @@ def main(chain):
     with open('../abis/Pair.json') as f:
         pair_abi = json.load(f)
 
-    minichef_contract = w3.eth.contract(w3.toChecksumAddress(minichef_address), abi=minichef_abi)
+    minichef_contract = w3.eth.contract(
+        w3.toChecksumAddress(minichef_address), abi=minichef_abi)
 
     for pool in pools_data:
-        pair_contract = w3.eth.contract(w3.toChecksumAddress(pool['pair']), abi=pair_abi)
+        pair_contract = w3.eth.contract(
+            w3.toChecksumAddress(pool['pair']), abi=pair_abi)
 
-        pool_info = minichef_contract.functions.poolInfo(int(pool['id'])).call()
-        slp_balance = pair_contract.functions.balanceOf(w3.toChecksumAddress(minichef_address)).call()
+        pool_info = minichef_contract.functions.poolInfo(
+            int(pool['id'])).call()
+        slp_balance = pair_contract.functions.balanceOf(
+            w3.toChecksumAddress(minichef_address)).call()
 
         print(f"pid: {pool['id']}")
         print(f'Acc Sushi per Share: {pool_info[0] / 1e12}')
         print(f'Balance: {slp_balance / 1e18}')
         print(f"Debt: {(pool_info[0] / 1e12) * (slp_balance / 1e18)}")
 
-    #for pool in pools_data:
+    # for pool in pools_data:
     #    print(pool['id'])
     #    print(pool['pair'])
 
@@ -93,12 +98,15 @@ def main(chain):
     print(f'\nTotal Debt: {total_debt / 10**decimals}')
     '''
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Process sushi debt for rewarders.")
+    parser = argparse.ArgumentParser(
+        description="Process sushi debt for rewarders.")
     parser.add_argument('--chain', help="chain name")
 
     args = parser.parse_args()
 
-    if args.chain == None : raise ValueError('Missing --chain in cmd arguments.')
+    if args.chain == None:
+        raise ValueError('Missing --chain in cmd arguments.')
 
     main(args.chain)
--- chef-reward-debt/calculate_reward_debt.py
+++ chef-reward-debt/calculate_reward_debt.py
@@ -5,32 +5,31 @@ import pandas as pd
 from web3 import Web3, HTTPProvider
 
 GRAPH_ENDPOINTS = {
-    'mainnet': 'https://api.thegraph.com/subgraphs/name/sushiswap/master-chefv2',
-    'polygon': 'https://api.thegraph.com/subgraphs/name/jiro-ono/minichef-staging-updates',
-    'xdai': 'https://api.thegraph.com/subgraphs/name/matthewlilley/xdai-minichef',
-    'harmony': 'https://sushi.graph.t.hmny.io/subgraphs/name/sushiswap/harmony-minichef',
-    'celo': 'https://api.thegraph.com/subgraphs/name/sushiswap/celo-minichef-v2',
-    'moonriver': 'https://api.thegraph.com/subgraphs/name/sushiswap/moonriver-minichef',
-    'arbitrum': 'https://api.thegraph.com/subgraphs/name/sushiswap/arbitrum-minichef',
-    'fuse': 'https://api.thegraph.com/subgraphs/name/sushiswap/fuse-minichef'
+    "mainnet": "https://api.thegraph.com/subgraphs/name/sushiswap/master-chefv2",
+    "polygon": "https://api.thegraph.com/subgraphs/name/jiro-ono/minichef-staging-updates",
+    "xdai": "https://api.thegraph.com/subgraphs/name/jiro-ono/gnosis-minichef-staging",
+    "harmony": "https://sushi.graph.t.hmny.io/subgraphs/name/sushiswap/harmony-minichef",
+    "celo": "https://api.thegraph.com/subgraphs/name/sushiswap/celo-minichef-v2",
+    "moonriver": "https://api.thegraph.com/subgraphs/name/sushiswap/moonriver-minichef",
+    "arbitrum": "https://api.thegraph.com/subgraphs/name/sushiswap/arbitrum-minichef",
+    "fuse": "https://api.thegraph.com/subgraphs/name/sushiswap/fuse-minichef",
 }
 
 RPC_ENDPOINTS = {
-    'mainnet': 'https://mainnet.infura.io/v3/a1b1da847a6840c7bbd718f6200a48c8',
-    'polygon': 'https://polygon-rpc.com/',
-    'xdai': 'https://rpc.xdaichain.com',
-    'harmony': 'https://api.harmony.one',
-    'celo': 'https://forno.celo.org',
-    'moonriver': 'https://rpc.moonriver.moonbeam.network',
-    'arbitrum': 'https://arb1.arbitrum.io/rpc',
-    'fuse': 'https://rpc.fuse.io'
+    "mainnet": "https://mainnet.infura.io/v3/a1b1da847a6840c7bbd718f6200a48c8",
+    "polygon": "https://polygon-rpc.com/",
+    "xdai": "https://rpc.gnosischain.com",
+    "harmony": "https://api.harmony.one",
+    "celo": "https://forno.celo.org",
+    "moonriver": "https://rpc.moonriver.moonbeam.network",
+    "arbitrum": "https://arb1.arbitrum.io/rpc",
+    "fuse": "https://rpc.fuse.io",
 }
 
+
 def main(chain, pid, decimals):
-
     graph_url = GRAPH_ENDPOINTS[chain]
-    print(f'Pulling data from {chain} graph endpoint.')
-
+    print(f"Pulling data from {chain} graph endpoint.")
 
     query = """query chefQuery($pid: String!) {
         pool(id: $pid) {
@@ -47,72 +46,79 @@ def main(chain, pid, decimals):
         }
     }
     """
-    variables = {'pid': str(pid)}
+    variables = {"pid": str(pid)}
 
-    result = requests.post(graph_url, json={'query': query, 'variables': variables})
+    result = requests.post(graph_url, json={"query": query, "variables": variables})
     data = json.loads(result.text)
 
-    df_data = data['data']['pool']['users']
+    df_data = data["data"]["pool"]["users"]
 
-    user_list = [
-        entry['address']
-        for entry in data['data']['pool']['users']
-    ]
+    user_list = [entry["address"] for entry in data["data"]["pool"]["users"]]
 
-    rewarder_addy = data['data']['pool']['rewarder']['id']
-    #print(data['data']['pool']['rewarder'])
-    reward_token_addy = data['data']['pool']['rewarder']['rewardToken']
-    print(f'Rewarder address: {rewarder_addy}')
+    rewarder_addy = data["data"]["pool"]["rewarder"]["id"]
+    # print(data['data']['pool']['rewarder'])
+    reward_token_addy = data["data"]["pool"]["rewarder"]["rewardToken"]
+    print(f"Rewarder address: {rewarder_addy}")
 
     w3 = Web3(Web3.HTTPProvider(RPC_ENDPOINTS[chain]))
 
-    print(f'RPC is connected: {w3.isConnected}')
-    print(f'Current block is: {w3.eth.blockNumber}')
+    print(f"RPC is connected: {w3.isConnected}")
+    print(f"Current block is: {w3.eth.blockNumber}")
 
-    with open('../abis/Rewarder.json') as f:
+    with open("../abis/Rewarder.json") as f:
         rewarder_abi = json.load(f)
 
-    with open('../abis/ERC20.json') as f:
+    with open("../abis/ERC20.json") as f:
         token_abi = json.load(f)
 
-
-    rewarder_contract = w3.eth.contract(w3.toChecksumAddress(rewarder_addy), abi=rewarder_abi)
-    reward_token_contract = w3.eth.contract(w3.toChecksumAddress(reward_token_addy), abi=token_abi)
+    rewarder_contract = w3.eth.contract(
+        w3.toChecksumAddress(rewarder_addy), abi=rewarder_abi
+    )
+    reward_token_contract = w3.eth.contract(
+        w3.toChecksumAddress(reward_token_addy), abi=token_abi
+    )
 
     total_debt = 0
     debt_dict = {}
     pending_users = []
 
-
+    i = 0
     for addy in user_list:
-        pending = rewarder_contract.functions.pendingToken(pid, w3.toChecksumAddress(addy)).call()
+        pending = rewarder_contract.functions.pendingToken(
+            pid, w3.toChecksumAddress(addy)
+        ).call()
         if pending == 0:
             continue
         total_debt += pending
 
-        debt_dict[addy] = (pending / 10**decimals)
+        debt_dict[addy] = pending / 10**decimals
         pending_users.append(addy)
 
-    rewarder_balance = reward_token_contract.functions.balanceOf(w3.toChecksumAddress(rewarder_addy)).call()
+    # rewarder_balance = reward_token_contract.functions.balanceOf(
+    #    w3.toChecksumAddress(rewarder_addy)
+    # ).call()
 
-    print('\nUser List:\n')
+    print("\nUser List:\n")
     print(pending_users)
-    print('\nUser Amounts:\n')
+    print("\nUser Amounts:\n")
     print(json.dumps(debt_dict, indent=1))
-    print(f'\nTotal Debt: {total_debt / 10**decimals}')
-    print(f'\nRewarder Balance: {rewarder_balance / 10**decimals}')
+    print(f"\nTotal Debt: {total_debt / 10**decimals}")
+    # print(f"\nRewarder Balance: {rewarder_balance / 10**decimals}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process sushi debt for rewarders.")
-    parser.add_argument('--chain', help="chain name")
-    parser.add_argument('--pid', help="pool id", type=int)
-    parser.add_argument('--decimals', help="reward token decimals", type=int)
+    parser.add_argument("--chain", help="chain name")
+    parser.add_argument("--pid", help="pool id", type=int)
+    parser.add_argument("--decimals", help="reward token decimals", type=int)
 
     args = parser.parse_args()
 
-    if args.chain == None : args.chain = 'mainnet'
-    if args.decimals == None : args.decimals = 18
-    if args.pid == None : raise ValueError('Missing --pid in cmd arguments.')
+    if args.chain == None:
+        args.chain = "mainnet"
+    if args.decimals == None:
+        args.decimals = 18
+    if args.pid == None:
+        raise ValueError("Missing --pid in cmd arguments.")
 
     main(args.chain, args.pid, args.decimals)
